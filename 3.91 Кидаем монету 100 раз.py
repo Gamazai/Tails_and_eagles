75a59cc1d7749fbf3d877c2927dab68c80dc1b4f
Gamazai <43391757+Gamazai@users.noreply.github.com>
2020-05-07 23:47:40 +0300

    Add files via upload

---
+++ 3.91 Кидаем монету 100 раз.py
@@ -0,0 +1,33 @@
+#Кидаем монету 100 раз
+print ("\tСейчас мы кинем монету 100раз и узнаем сколько выпало орлов и решек")
+import random
+broski = 10
+tails=0
+eagles=0
+cast=0
+attempt=0
+
+
+while attempt < broski:
+    cast=random.randint(1,2)
+    #print(cast)
+    if cast == 1:
+        print(attempt+1, ": Решка")
+        tails +=1
+    if cast==2:
+        print(attempt+1, ": Орёл")
+        eagles +=1
+
+    attempt += 1
+
+Dolia_tails = tails/broski
+Dolia_eagles = eagles/broski
+
+print("Мы кинули монету", broski, " раз и у нас выпало", "Решек: ", tails, "Орлов: ",
+eagles)
+
+print("Решка:", Dolia_tails*100, "%")
+print("Орёл:", Dolia_eagles*100, "%")
+
+
+input("\n\n")
